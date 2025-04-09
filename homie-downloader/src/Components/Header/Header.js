1bbcb62ff1f5b7234be9fae33bab414a73a232d9
Ihor Kachmar <ihor.kachmar.oi.2023@lpnu.ua>
2025-04-09 21:03:37 +0300

    Commit Message

--- homie-downloader/src/Components/Header/Header.js
+++ homie-downloader/src/Components/Header/Header.js
@@ -1,11 +1,30 @@
 import styles from './Header.module.css';
 import SearchBar from '../SearchBar/SearchBar';
 import { FiUser, FiBell, FiMenu } from 'react-icons/fi';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsMenuOpen(false);
+            }
+        };
+
+        if (isMenuOpen) {
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isMenuOpen]);
 
     return (
         <header className={styles.header}>
@@ -20,7 +39,6 @@ const Header = () => {
             </div>
 
             <div className={styles.rightSection}>
-
                 <button className={styles.notificationButton}>
                     <FiBell size={20} />
                     <span className={styles.notificationBadge}>3</span>
@@ -28,8 +46,8 @@ const Header = () => {
                 <div className={styles.userAvatar} onClick={() => setIsMenuOpen(!isMenuOpen)}>
                     <FiUser size={20} />
                     {isMenuOpen && (
-                        <div className={styles.userDropdown}>
-                            <Link to="/account" className={styles.dropdownItem}>Profile</Link>
+                        <div className={styles.userDropdown} ref={dropdownRef}>
+                            <Link to="/account" className={styles.dropdownItem} onClick={() => setIsMenuOpen(false)}>Profile</Link>
                             <div className={styles.dropdownItem}>Settings</div>
                             <div className={styles.dropdownItem}>Logout</div>
                         </div>
