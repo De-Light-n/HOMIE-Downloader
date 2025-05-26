178ed76b7c61f4424753ba0c49963a23437a5376
Ruslan-Pavlo Maniuk <ruslanmanjuk@gmail.com>
2025-05-26 20:57:12 +0300

    delete footer bottom

--- homie-downloader/src/Components/Footer/Footer.js
+++ homie-downloader/src/Components/Footer/Footer.js
@@ -63,12 +63,6 @@ function Footer() {
                 </div>
             </div>
 
-            {/* Нижня частина */}
-            <div className="footer-bottom">
-                <p>© Homie Downloader 2025 <Link to="/privacy">Політика конфіденційності</Link></p>
-                <p>ДИЗАЙН ТА РОЗРОБКА ⚡ <a href="https://webstudio.example.com">WEBSTUDIO</a></p>
-            </div>
-
             {/* Великий напис на фоні */}
             <div className="footer-bg-text">Homie</div>
         </footer>
