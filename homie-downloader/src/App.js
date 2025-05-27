c4e2741dca82310fcd8d60abd8af048c95352a25
Ihor Kachmar <ihor.kachmar.oi.2023@lpnu.ua>
2025-05-27 18:39:03 +0300

    Профіль зміни

--- homie-downloader/src/App.js
+++ homie-downloader/src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './Components/Firebase/AuthContext';
+// --- START OF FILE App.js ---
 
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; // Додано useLocation
+import { AuthProvider } from './Components/Firebase/AuthContext';
 
 import Home from './Pages/Home/Home';
 import AccountPage from './Pages/AccountPage/AccountPage';
@@ -16,8 +17,7 @@ import Footer from './Components/Footer/Footer';
 
 import Background from './Components/Background';
 import ScrollToTop from './Components/ScrollToTop';
-import {ThemeProvider} from './Components/ThemeContext';
-
+import { ThemeProvider } from './Components/ThemeContext';
 
 import './Styles/themes.css';
 // TODO: Maybe Footer
@@ -28,10 +28,35 @@ import './Styles/themes.css';
 // TODO: IF we have time, add more themes
 // TODO: Перевести сайт на одну мову
 // TODO: в сторінці акаунта надпис My account без стилю
-// TODO: Забрати останній візит на сайт з акаунта 
+// TODO: Забрати останній візит на сайт з акаунта
 // TODO: Доробити аналітику
 // TODO: ДОРОБИТИ ФУТЕР
 
+// Компонент, що містить логіку відображення Header, Routes та Footer
+function AppLayout() {
+    const location = useLocation();
+    const pathsWithoutFooter = ['/login', '/signup'];
+
+    // Перевіряємо, чи поточний шлях НЕ знаходиться у списку шляхів без футера
+    const showFooter = !pathsWithoutFooter.includes(location.pathname.toLowerCase());
+
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/video/details" element={<VideoDetailsPage />} />
+                <Route path="/account" element={<AccountPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/aboutus" element={<AboutUs />} />
+                <Route path="/guide" element={<Guide />} />
+                <Route path="/letschat" element={<LetsChat />} />
+            </Routes>
+            {showFooter && <Footer />}
+        </>
+    );
+}
 
 function App() {
     return (
@@ -40,18 +65,8 @@ function App() {
                 <Background />
                 <ScrollToTop />
                 <AuthProvider>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/video/details" element={<VideoDetailsPage />} />
-                        <Route path="/account" element={<AccountPage />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/aboutus" element={<AboutUs />} />
-                        <Route path="/guide" element={<Guide />} />
-                        <Route path="/letschat" element={<LetsChat />} />
-                    </Routes>
-                    <Footer />
+                    {/* Використовуємо AppLayout замість прямого рендерингу Header, Routes, Footer */}
+                    <AppLayout />
                 </AuthProvider>
             </ThemeProvider>
         </Router>
@@ -59,3 +74,4 @@ function App() {
 }
 
 export default App;
+// --- END OF FILE App.js ---
